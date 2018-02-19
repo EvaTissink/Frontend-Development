509af3cc099b879ee94343d1ca60ad31c2f455a1
EvaTissink <eva.tissink@gmail.com>
2018-02-19 12:23:31 +0100

    Basis code

---
+++ details studenthuizen versie 1/js/main.js
@@ -0,0 +1,9 @@
+var pijl = document.querySelector('article header button');
+var info = document.querySelector('article header div');
+
+
+pijl.addEventListener("click",function(){
+    info.classList.toggle('drop');
+  }
+);
+
