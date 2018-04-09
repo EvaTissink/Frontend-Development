f9372410bf03fb4d24696d942317eb7aee090164
EvaTissink <eva.tissink@gmail.com>
2018-04-09 21:12:00 +0200

    oké

--- opdracht 3/versie 5/js/main.js
+++ opdracht 3/versie 5/js/main.js
@@ -3,7 +3,7 @@
 var header = document.querySelector('header');
 var section = document.querySelector('section');
 
-var requestURL = 'json/main.json';
+var requestURL = '../json/main.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
