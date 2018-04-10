2716918a1110358d1efc8e3d3b282a47d93d8b39
EvaTissink <eva.tissink@gmail.com>
2018-04-10 15:55:27 +0200

    json

--- opdracht 3/versie 5/js/main.js
+++ opdracht 3/versie 5/js/main.js
@@ -4,7 +4,7 @@ var header = document.querySelector('header');
 var section = document.querySelector('section');
 
 //var requestURL = 'http://dennistel.nl/movies';
-var requestURL = 'https://tomwesterhof.github.io/Frontend-voor-Designers/Opdracht%203/v3/movies.json';
+var requestURL = 'https://tomwesterhof.github.io/Frontend-voor-Designers/Opdracht%203/v3/json/movies.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
